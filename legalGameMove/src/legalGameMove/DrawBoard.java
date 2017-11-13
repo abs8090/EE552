cf76bb1eaada929248c91bc48d6cb44884cf07a5
abs8090 <km.5432@hotmail.com>
2017-11-12 19:56:55 -0500

    shorter code to draw board with boxes, using prof's note

--- legalGameMove/src/legalGameMove/DrawBoard.java
+++ legalGameMove/src/legalGameMove/DrawBoard.java
@@ -16,41 +16,32 @@ public class DrawBoard extends JPanel{
 	public void paint(Graphics g){
 		int winSize = getWidth();
 		int count = 0;
-		while(count <= 10){
-				
-			if((count % 2) == 0){
-				
+		boolean odd = true;
+		
+		while(count <= 10){		
+			
 			for(int i = 0; i <= winSize; i++){				
 				for(int j = 0; j <= winSize; j++){
+			
+					if(odd){
+						g.setColor(Color.black);
+						g.fillRect(r , c, boxSize, boxSize);
+						r+= 80;
 				
-					g.setColor(Color.black);
-					g.fillRect(r , c, boxSize, boxSize);
-
-					r+= 80;
+					}else{
 						g.setColor(Color.white);
 						g.fillRect(r, c, boxSize, boxSize);
-					r+= 80;					
-				}
-			}
+						r+= 80;
 			
-		}else{
-			
-			for(int i = 0; i <= winSize; i++){				
-				for(int j = 0; j <= winSize; j++){
-				
-					g.setColor(Color.white);
-					g.fillRect(r , c, boxSize, boxSize);
-
-					r+= 80;
-						g.setColor(Color.black);
-						g.fillRect(r, c , boxSize, boxSize);
-					r+= 80;					
 				}
+				odd = !odd;
 			}
-		}
-			r = 0;
-			c+= 80; 
-			count++;
+			odd = !odd;
+			}
+		r = 0;
+		c+= 80; 
+		count++;
+		odd = !odd;
 	}
 		
 
