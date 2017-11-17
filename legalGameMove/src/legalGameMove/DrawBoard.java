ec3e8c213bcdfa8aff8f0aa923b325a2b77beda1
abs8090 <km.5432@hotmail.com>
2017-11-16 19:44:22 -0500

    following best practices

--- legalGameMove/src/legalGameMove/DrawBoard.java
+++ legalGameMove/src/legalGameMove/DrawBoard.java
@@ -22,39 +22,39 @@ public class DrawBoard extends JPanel{
 				if(odd){
 					g.setColor(Color.white);
 					g.fillRect(r , c, j * boxSize, j * boxSize);
-					System.out.println("row white: " + (c / 80));
-					System.out.println("col white: " + (r / 80));
-					BoardState.bs[(c / 80)][(r / 80)] = BoardState.boardState.ILLEGAL;
+					System.out.println("row white: " + (c / boxSize));
+					System.out.println("col white: " + (r / boxSize));
+					BoardState.bs[(c / boxSize)][(r / boxSize)] = BoardState.boardState.ILLEGAL;
 					
-					c+= 80;
+					c+= boxSize;
 					odd = !odd;
 					
 				}else{
 					g.setColor(Color.black);
 					g.fillRect(r, c, j * boxSize, j * boxSize);
 					
-					System.out.println("row black: " + (c / 80));
-					System.out.println("col black: " + (r / 80));
-					BoardState.bs[(c / 80)][(r / 80)] = BoardState.boardState.AVAILABLE;
+					System.out.println("row black: " + (c / boxSize));
+					System.out.println("col black: " + (r / boxSize));
+					BoardState.bs[(c / boxSize)][(r / boxSize)] = BoardState.boardState.AVAILABLE;
 					
-					if(c == 240 || c == 320){
+					if(c == (boxSize * 3) || c == (boxSize * 4)){
 							
 					}else{
 							
 						if( c < 240){
 							g.setColor(Color.RED);
 							dc.setPoints(r + 10,  c + 10 , 60, 60);
-							BoardState.bs[(c / 80)][(r / 80)] = BoardState.boardState.TAKEN;
+							BoardState.bs[(c / boxSize)][(r / boxSize)] = BoardState.boardState.TAKEN;
 							dc.draw(g);
 						
 						}else if( c > 320){
 							g.setColor(Color.BLUE);
 							dc.setPoints(r + 10,  c + 10 , 60, 60);
-							BoardState.bs[(c / 80)][(r / 80)] = BoardState.boardState.TAKEN;
+							BoardState.bs[(c / boxSize)][(r / boxSize)] = BoardState.boardState.TAKEN;
 							dc.draw(g);
 							}
 						}
-						c+= 80;
+						c+= boxSize;
 						odd = !odd;
 				}
 
